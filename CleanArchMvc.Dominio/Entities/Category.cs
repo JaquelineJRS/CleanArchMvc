7791d4ebeb54a28ad8e375ecf44af08651d11e55
Jaqueline Rafaela <jaqueline.r_santos@outlook.com>
2023-01-16 21:32:19 -0300

    feat: enriquecendo o modelo com as validações das propriedades

--- CleanArchMvc.Dominio/Entities/Category.cs
+++ CleanArchMvc.Dominio/Entities/Category.cs
@@ -1,13 +1,35 @@
-﻿using System.Collections.Generic;
+﻿using CleanArchMvc.Dominio.Validation;
+using System;
+using System.Collections.Generic;
 
 namespace CleanArchMvc.Dominio.Entities
 {
-    public class Category
+    //Classes sealed não podem ser herdadas
+    public sealed class Category
     {
-        public int CategoryId { get; set; }
-        public string Name { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }        
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Valor inválido para o Id, não pode ser menor do que zero.");
+            Id = id;
+            ValidateDomain(name);
+        }
 
         //Neste caso estamos indicando que a classe categoria pode conter uma coleção de produtos
         public ICollection<Product> Products { get; set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name), "Nome inválido. name.Name é obrigatório.");
+
+            DomainExceptionValidation.When(name.Length < 3, "Nome inválido. O nome não pode conter menos que três caracteres.");
+        }
     }
 }
--- CleanArchMvc.Dominio/Entities/Product.cs
+++ CleanArchMvc.Dominio/Entities/Product.cs
@@ -1,13 +1,14 @@
 ﻿namespace CleanArchMvc.Dominio.Entities
 {
-    public class Product
+    //Classes sealed não podem ser herdadas
+    public sealed class Product
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
-        public string Description { get; set; }
-        public decimal Price { get; set; }
-        public int Stock { get; set; }
-        public string Image { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
+        public string Description { get; private set; }
+        public decimal Price { get; private set; }
+        public int Stock { get; private set; }
+        public string Image { get; private set; }
 
         //Aqui definimos a propriedade de navegação para que o EF entenda que é preciso criar uma tabela de relação entre as duas classes
         public int CategoryId { get; set; }
