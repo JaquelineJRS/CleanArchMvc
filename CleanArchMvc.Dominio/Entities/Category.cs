b42c7a77cfa665dcb5aceeff5baf461959a50be1
Jaqueline Rafaela <jaqueline.r_santos@outlook.com>
2023-01-16 21:19:41 -0300

    feat: criado a primeira versão para o modelo de dominio anêmico

---
+++ CleanArchMvc.Dominio/Entities/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace CleanArchMvc.Dominio.Entities
+{
+    public class Category
+    {
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+
+        //Neste caso estamos indicando que a classe categoria pode conter uma coleção de produtos
+        public ICollection<Product> Products { get; set; }
+    }
+}
---
+++ CleanArchMvc.Dominio/Entities/Product.cs
@@ -0,0 +1,16 @@
+﻿namespace CleanArchMvc.Dominio.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public decimal Price { get; set; }
+        public int Stock { get; set; }
+        public string Image { get; set; }
+
+        //Aqui definimos a propriedade de navegação para que o EF entenda que é preciso criar uma tabela de relação entre as duas classes
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
