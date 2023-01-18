8377ef36b5b3f6e35f46dbe60b3123897d45f3d4
Jaqueline Rafaela <jaqueline.r_santos@outlook.com>
2023-01-18 16:36:39 -0300

    feat: criação das interfaces de repositório

---
+++ CleanArchMvc.Dominio/Interfaces/IProductRepository.cs
@@ -0,0 +1,16 @@
+﻿using CleanArchMvc.Dominio.Entities;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Dominio.Interfaces
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<Product>> GetProductsAsync();
+        Task<Category> GetByIdAsync(int? id);
+
+        Task<Category> CreateAsync(Category category);
+        Task<Category> UpdateAsync(Category category);
+        Task<Category> RemoveAsync(Category category);
+    }
+}
