826e6cad12a51ebda5023e16a0e9b4813fb8798c
Jaqueline Rafaela <jaqueline.r_santos@outlook.com>
2023-03-06 21:20:04 -0300

    feat: criação da camada de infraestrutura, configurando as entidade utilizando Fluent API

---
+++ CleanArchMvc.Infra.Data/EntitiesConfiguration/ProductConfiguration.cs
@@ -0,0 +1,34 @@
+﻿using CleanArchMvc.Dominio.Entities;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Infra.Data.EntitiesConfiguration
+{
+    public class ProductConfiguration : IEntityTypeConfiguration<Product>
+    {
+        public void Configure(EntityTypeBuilder<Product> builder)
+        {
+            builder.HasKey(p => p.Id);
+            builder.Property(p => p.Name)
+                .HasMaxLength(100)
+                .IsRequired();
+
+            builder.Property(p => p.Description)
+                .HasMaxLength(200)
+                .IsRequired();
+
+            builder.Property(p => p.Price)
+                .HasPrecision(10, 2);
+
+            builder.HasOne(p => p.Category)
+                .WithMany(c => c.Products)
+                .HasForeignKey(p => p.CategoryId);
+        }
+    }
+}
