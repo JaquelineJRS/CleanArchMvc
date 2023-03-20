d7199d44f3fd06c472e3ec4f4d7a943cb6d695d9
Jaqueline Rafaela <jaqueline.r_santos@outlook.com>
2023-03-19 21:55:08 -0300

    feat: criação da página e método para adicionar uma nova categoria.

--- CleanArchMvc.WebUI/Controllers/CategoriesController.cs
+++ CleanArchMvc.WebUI/Controllers/CategoriesController.cs
@@ -1,4 +1,5 @@
-﻿using CleanArchMvc.Application.Interfaces;
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
 using Microsoft.AspNetCore.Mvc;
 using System.Threading.Tasks;
 
@@ -18,6 +19,25 @@ namespace CleanArchMvc.WebUI.Controllers
         {
             var categories = await _categoryService.GetCategories();
             return View(categories);
+        }
+
+
+
+        [HttpGet]
+        public IActionResult Create()
+        {            
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CategoryDTO category)
+        {
+            if (ModelState.IsValid)
+            {
+                await _categoryService.Add(category);
+                return RedirectToAction(nameof(Index));
+            }
+            return View(category);
         }
     }
 }
