0330767b840736df211eb1b2a90ee0161f038410
Jaqueline Rafaela <jaqueline.r_santos@outlook.com>
2023-04-27 18:27:45 -0300

    feat: criando as interfaces na camada domain

--- CleanArchMvc.Infra.Data/Migrations/20230427212054_AddIndentityTables.Designer.cs
+++ CleanArchMvc.Infra.Data/Migrations/20230427212054_AddIndentityTables.Designer.cs
@@ -4,14 +4,16 @@ using CleanArchMvc.Infra.Data.Context;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace CleanArchMvc.Infra.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20230427212054_AddIndentityTables")]
+    partial class AddIndentityTables
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
