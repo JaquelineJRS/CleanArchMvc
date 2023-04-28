a3356860c049b166bc5595a442712b8a6900d496
Jaqueline Rafaela <jaqueline.r_santos@outlook.com>
2023-04-28 20:40:22 -0300

    feat: configurando os serviços na startup layout: incluind a tela de registro feat: adicionando autorização de acesso para admin em um método do controlador do produto

--- CleanArchMvc.WebUI/Controllers/ProductsController.cs
+++ CleanArchMvc.WebUI/Controllers/ProductsController.cs
@@ -1,5 +1,6 @@
 ﻿using CleanArchMvc.Application.DTOs;
 using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
@@ -71,6 +72,7 @@ namespace CleanArchMvc.WebUI.Controllers
             return View(product);
         }
 
+        [Authorize(Roles = "Admin")]
         [HttpGet]
         public async Task<IActionResult> Delete(int? id)
         {
