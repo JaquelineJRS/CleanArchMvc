fd2599480e31d7a3d42d26a96ab0d1db64793f01
Jaqueline Rafaela <jaqueline.r_santos@outlook.com>
2023-04-28 22:19:11 -0300

    feat: implementando o GET para a lista de categorias da API

---
+++ ClearArchMvc.Api/Controllers/CategoriesController.cs
@@ -0,0 +1,28 @@
+﻿using CleanArchMvc.Application.DTOs;
+using CleanArchMvc.Application.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CategoriesController : ControllerBase
+    {
+        private readonly ICategoryService _categoryService;
+
+        public CategoriesController(ICategoryService categoryService)
+        {
+            _categoryService = categoryService;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<CategoryDTO>>> GetAsync()
+        {
+            var categoryList = await _categoryService.GetCategories();
+            if (categoryList == null) return NotFound("Categories not found.");
+            return Ok(categoryList);
+        }
+    }
+}
