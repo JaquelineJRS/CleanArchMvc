f687a5fd5baa040ee28d373469b548c7555cef11
Jaqueline Rafaela <jaqueline.r_santos@outlook.com>
2023-04-30 22:11:49 -0300

    feat: implementação do controller do token com o método simplificado de login, ainda sem a autenticação

---
+++ ClearArchMvc.Api/Controllers/TokenController.cs
@@ -0,0 +1,39 @@
+﻿using CleanArchMvc.Api.Models;
+using CleanArchMvc.Dominio.Account;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Threading.Tasks;
+
+namespace CleanArchMvc.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TokenController : ControllerBase
+    {
+        private readonly IAuthenticate _authenticate;
+
+        public TokenController(IAuthenticate authenticate)
+        {
+            _authenticate = authenticate ??
+                throw new ArgumentNullException(nameof(authenticate));
+        }
+
+        [HttpPost("LoginModel")]
+        public async Task<ActionResult<UserTokenModel>> Login([FromBody] LoginModel loginModel)
+        {
+            var result = await _authenticate.Authenticate(loginModel.Email, loginModel.Password);
+            if (result)
+            {
+                //loginModel.GenareteToken();
+                return Ok("Login successful.");
+            }
+            else
+            {
+                ModelState.AddModelError(string.Empty, "Invalid login.");
+                return BadRequest(ModelState);
+            }
+        }
+        
+    }
+}
+
